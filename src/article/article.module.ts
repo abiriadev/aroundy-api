fe36e30d36383f564c156ba801499953d1988887
Byeongchan <63byeongchan@gmail.com>
2024-04-14 22:47:14 +0900

    fix: lint fix

--- src/article/article.module.ts
+++ src/article/article.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+
 import { ArticleService } from './article.service';
 import { ArticleController } from './article.controller';
 
 @Module({
   providers: [ArticleService],
-  controllers: [ArticleController]
+  controllers: [ArticleController],
 })
 export class ArticleModule {}
