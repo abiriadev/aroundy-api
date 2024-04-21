aa3bdef20f809f64624d05e7de2c8dfa0e56c038
Byeongchan <63byeongchan@gmail.com>
2024-04-21 13:50:17 +0900

    feat: account 도메인 스웨거 연동

--- src/account/account.controller.ts
+++ src/account/account.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+import { Public } from 'src/middleware/decorator/public.decorator';
+
+import { LoginRequestDTO } from './dtos/account.request.dto';
+import { TokenResponseDTO } from './dtos/account.response.dto';
+
+@ApiTags('회원관리 API')
 @Controller('account')
-export class AccountController {}
+export class AccountController {
+  @ApiOperation({ summary: '로그인' })
+  @ApiBody({ type: LoginRequestDTO })
+  @ApiResponse({
+    status: 200,
+    description: '로그인 성공',
+    type: TokenResponseDTO,
+  })
+  @ApiResponse({
+    status: 401,
+    description: '잘못된 요청입니다.',
+  })
+  @Public()
+  @Post('/login')
+  login(@Body() body: LoginRequestDTO): void {}
+}
