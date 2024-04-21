c74efeda100c8b87bdf6851a97c1fa932690c282
Byeongchan <63byeongchan@gmail.com>
2024-04-21 12:15:10 +0900

    chore: article 도메인 제거

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,12 @@ import { Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
 import { ConfigModule } from '@nestjs/config';
 
-import { ArticleModule } from './article/article.module';
 import { CommonModule } from './common/common.module';
 import { DatabaseModule } from './database/database.module';
 import { AuthGuard } from './middleware/guard/auth.guard';
 
 @Module({
-  imports: [ArticleModule, CommonModule, DatabaseModule, ConfigModule],
+  imports: [CommonModule, DatabaseModule, ConfigModule],
   providers: [
     {
       provide: APP_GUARD,
