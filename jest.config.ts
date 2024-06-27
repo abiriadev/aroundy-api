69b404428099af116751489f9bd8df4e386579dd
Abiria <abiria.dev@gmail.com>
2024-06-27 16:45:01 +0900

    build: extract jest config, use swc instead of ts-jest

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+export default {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s?$': ['@swc/jest'],
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+};
