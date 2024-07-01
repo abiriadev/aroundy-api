e8d61d835c3fed5326d601e01916ecf57a5346c3
Abiria <abiria.dev@gmail.com>
2024-07-01 13:43:21 +0900

    refactor: use refactored service

--- src/company/company.controller.ts
+++ src/company/company.controller.ts
@@ -2,40 +2,37 @@ import {
   Controller,
   Get,
   Post,
-  Put,
   Delete,
   Body,
   Param,
+  Patch,
 } from '@nestjs/common';
-import { CompanyService } from './company.service';
-import { Company } from './company.entity';
+import { CompanyService, CreateCompany } from './company.service';
 
 @Controller('companies')
 export class CompanyController {
   constructor(private readonly companyService: CompanyService) {}
 
   @Get()
-  findAll(): Promise<Company[]> {
-    return this.companyService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string): Promise<Company> {
-    return this.companyService.findOne(id);
+  async findAll() {
+    return this.companyService.findAll({});
   }
 
   @Post()
-  create(@Body() company: Company): Promise<Company> {
+  async create(@Body() company: CreateCompany) {
     return this.companyService.create(company);
   }
 
-  @Put(':id')
-  update(@Param('id') id: string, @Body() company: Company): Promise<Company> {
+  @Patch(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() company: Partial<CreateCompany>,
+  ) {
     return this.companyService.update(id, company);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string): Promise<Company> {
+  async remove(@Param('id') id: string) {
     return this.companyService.remove(id);
   }
 }
