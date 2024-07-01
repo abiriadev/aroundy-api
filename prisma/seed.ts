fb34475083e2b248c068f73d6dcb5b980081e54d
Abiria <abiria.dev@gmail.com>
2024-07-01 12:50:04 +0900

    feat: setup seeding script

---
+++ prisma/seed.ts
@@ -0,0 +1,33 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const categories = ['팝업스토어', '뷰티', '식품', '축제', '서비스'];
+  const tags = ['New', '이벤트', '할인'];
+
+  await prisma.$transaction([
+    ...categories.map((name) =>
+      prisma.category.upsert({
+        where: { name },
+        update: { name },
+        create: { name },
+      }),
+    ),
+    ...tags.map((name) =>
+      prisma.category.upsert({
+        where: { name },
+        update: { name },
+        create: { name },
+      }),
+    ),
+  ]);
+}
+
+main()
+  .then(prisma.$disconnect)
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
