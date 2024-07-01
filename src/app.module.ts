db78c55e637d6c3f0e7d01ca2c072b08e12849b4
Abiria <abiria.dev@gmail.com>
2024-07-01 16:00:04 +0900

    refactor: fine main

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,21 @@
 import { Module } from '@nestjs/common';
-import { UserModule } from './user/user.module';
 import { CategoryModule } from './category/category.module';
 import { CompanyModule } from './company/company.module';
 import { PostModule } from './post/post.module';
 import { TagModule } from './tag/tag.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
-import { ConfigModule } from '@nestjs/config';
-import { configConfig } from './config/config.config';
 import { PrismaModule } from 'nestjs-prisma';
 import { prismaConfig } from './prisma.config';
+import { ConfigModule } from './config/config.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(configConfig),
+    ConfigModule.forRoot(),
     PrismaModule.forRootAsync(prismaConfig),
-    UserModule,
-    CategoryModule,
-    CompanyModule,
     PostModule,
+    CompanyModule,
+    CategoryModule,
     TagModule,
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'uploads'),
