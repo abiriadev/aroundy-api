5e3a23786589f5763d8689ba3317c0f806e228c3
Abiria <abiria.dev@gmail.com>
2024-07-01 22:06:21 +0900

    test: add passing tests for known points on korea map

---
+++ src/post/kakao-map.service.spec.ts
@@ -0,0 +1,38 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { Coordinate, KakaoMapService } from './kakao-map.service';
+import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from 'src/config/config.module';
+
+describe('KakaoMapService', () => {
+  let service: KakaoMapService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [ConfigModule.forRoot(), HttpModule],
+      providers: [KakaoMapService],
+    }).compile();
+
+    service = module.get<KakaoMapService>(KakaoMapService);
+  });
+
+  it('should find address from the coordinate', async () => {
+    const p1: Coordinate = [37.51264278891025, 127.10246789395465]; // 롯데월드타워
+    const p2: Coordinate = [35.86919238091584, 128.5924288991289]; // 스타벅스 대구종로고택점
+    const p3: Coordinate = [37.63057678520386, 126.79590550302485]; // 메르세데스 벤츠 내곡 서비스센터
+
+    expect(await service.coordinateToAddress(p1)).toHaveProperty(
+      'address',
+      '서울특별시 송파구 올림픽로 300',
+    );
+
+    expect(await service.coordinateToAddress(p2)).toHaveProperty(
+      'address',
+      '대구 중구 남일동 129-5',
+    );
+
+    expect(await service.coordinateToAddress(p3)).toHaveProperty(
+      'address',
+      '경기 고양시 덕양구 내곡동 488-14',
+    );
+  });
+});
