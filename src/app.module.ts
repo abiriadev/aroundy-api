78bd5542e32a2539656e513245bf69109cf6855d
Abiria <abiria.dev@gmail.com>
2024-07-01 16:00:26 +0900

    refactor: get rid of static serving

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,6 @@ import { CategoryModule } from './category/category.module';
 import { CompanyModule } from './company/company.module';
 import { PostModule } from './post/post.module';
 import { TagModule } from './tag/tag.module';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
 import { PrismaModule } from 'nestjs-prisma';
 import { prismaConfig } from './prisma.config';
 import { ConfigModule } from './config/config.module';
@@ -17,9 +15,6 @@ import { ConfigModule } from './config/config.module';
     CompanyModule,
     CategoryModule,
     TagModule,
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'uploads'),
-    }),
   ],
 })
 export class AppModule {}
