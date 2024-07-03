cd7b26d035ffb6d43d156e2ece43e2756196f41b
Abiria <abiria.dev@gmail.com>
2024-07-03 16:18:40 +0900

    feat: use kysely extension, define extended prisma client type

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,14 @@ import { CategoryModule } from './category/category.module';
 import { CompanyModule } from './company/company.module';
 import { PostModule } from './post/post.module';
 import { TagModule } from './tag/tag.module';
-import { PrismaModule } from 'nestjs-prisma';
+import { CustomPrismaModule, PrismaModule } from 'nestjs-prisma';
 import { prismaConfig } from './prisma.config';
 import { ConfigModule } from './config/config.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    PrismaModule.forRootAsync(prismaConfig),
+    CustomPrismaModule.forRootAsync(prismaConfig),
     PostModule,
     CompanyModule,
     CategoryModule,
--- src/prisma.config.ts
+++ src/prisma.config.ts
@@ -1,13 +1,46 @@
-import { PrismaModuleAsyncOptions } from 'nestjs-prisma';
+import {
+  CustomPrismaModuleAsyncOptions,
+  CustomPrismaService,
+} from 'nestjs-prisma';
 import { DbConfig } from './config/config.service';
 import { ConfigModule } from './config/config.module';
+import kyselyExtension from 'prisma-extension-kysely';
+import { PrismaClient } from '@prisma/client';
+import type { DB } from '../prisma/kysely/types';
+import {
+  Kysely,
+  PostgresAdapter,
+  PostgresIntrospector,
+  PostgresQueryCompiler,
+} from 'kysely';
+
+const extendedPrismaClientFactory = (dbConfigService: DbConfig) =>
+  new PrismaClient({
+    datasourceUrl: `postgresql://${dbConfigService.db_user}:${dbConfigService.db_password}@${dbConfigService.db_host}:${dbConfigService.db_port}/${dbConfigService.db_database}`,
+  }).$extends(
+    kyselyExtension({
+      kysely: (driver) =>
+        new Kysely<DB>({
+          dialect: {
+            createDriver: () => driver,
+            createAdapter: () => new PostgresAdapter(),
+            createIntrospector: (db) => new PostgresIntrospector(db),
+            createQueryCompiler: () => new PostgresQueryCompiler(),
+          },
+          plugins: [],
+        }),
+    }),
+  );
+
+export type ExtendedPrismaClient = ReturnType<
+  typeof extendedPrismaClientFactory
+>;
+
+export type ExtendedPrismaService = CustomPrismaService<ExtendedPrismaClient>;
 
 export const prismaConfig = {
+  name: 'PrismaService',
   imports: [ConfigModule],
   inject: [DbConfig],
-  useFactory: (dbConfigService: DbConfig) => ({
-    prismaOptions: {
-      datasourceUrl: `postgresql://${dbConfigService.db_user}:${dbConfigService.db_password}@${dbConfigService.db_host}:${dbConfigService.db_port}/${dbConfigService.db_database}`,
-    },
-  }),
-} satisfies PrismaModuleAsyncOptions;
+  useFactory: extendedPrismaClientFactory,
+} satisfies CustomPrismaModuleAsyncOptions<ExtendedPrismaClient>;
