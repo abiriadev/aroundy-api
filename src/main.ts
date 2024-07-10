b09048d464868b4a18d892600e1be36a9b844a6e
Abiria <abiria.dev@gmail.com>
2024-07-11 02:50:24 +0900

    feat: cutify openapi doc

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,30 @@ import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@/config/config.service';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const { port } = app.get(ConfigService.Network);
+  const { port, host } = app.get(ConfigService.Network);
+
+  SwaggerModule.setup(
+    'openapi',
+    app,
+    SwaggerModule.createDocument(
+      app,
+      new DocumentBuilder()
+        .setTitle('Aroundy')
+        .setDescription('Aroundy 앱 및 어드민 페이지 공용 API')
+        .setVersion('phase1')
+        .addServer(`https://${host}:${port}`, 'production server')
+        .setExternalDoc(
+          'Notion',
+          'https://www.notion.so/Aroundy-550556a8fa18413ea0e0bce1b415eb17',
+        )
+        .build(),
+    ),
+  );
 
   await app.listen(port);
   console.log(`Application is running on: ${port}`);
