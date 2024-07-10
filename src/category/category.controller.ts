52351d3b3055a0700c700517e17c695acd9f2da4
Abiria <abiria.dev@gmail.com>
2024-07-11 03:00:35 +0900

    docs: document category api

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,16 +1,28 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get } from '@nestjs/common';
 import { CategoryService } from './category.service';
-import { CreateCategoryDto } from './category.dto';
+import { CategoryDto } from './category.dto';
 
 @Controller('categories')
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
 
   /**
+   * 모든 카테고리 조회.
+   *
+   * 메뉴 바에서 카테고리 목록을 표시할 때 사용합니다.
+   *
+   * 기본적으로 각 카테고리가 생성된 순서대로 정렬되어 반환됩니다.
+   */
+  @Get()
+  async findAll(): Promise<Array<CategoryDto>> {
+    return await this.categoryService.findAll();
+  }
+
+  /**
    * 카테고리 추가.
    */
   @Post()
-  async create(@Body() category: CreateCategoryDto) {
+  async create(@Body() category: CategoryDto.Create) {
     await this.categoryService.create(category);
   }
 }
