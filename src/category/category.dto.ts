1604a91f08a9f51d8e214c0a2815bc2a2a75451a
Abiria <abiria.dev@gmail.com>
2024-07-11 03:10:41 +0900

    refactor: redefine category DTO

--- src/category/category.dto.ts
+++ src/category/category.dto.ts
@@ -1,9 +1,17 @@
-export class CreateCategoryDto {
+import { OmitType } from '@nestjs/swagger';
+
+export class CategoryDto {
+  /**
+   * 카테고리 고유 ID
+   */
+  id: string;
+
+  /**
+   * 카테고리 이름
+   */
   name: string;
 }
 
-export class CategoryDto extends CreateCategoryDto {
-  id: string;
-  createdAt: Date;
-  updatedAt: Date;
+export namespace CategoryDto {
+  export class Create extends OmitType(CategoryDto, ['id'] as const) {}
 }
