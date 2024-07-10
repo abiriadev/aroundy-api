c7be597bd0301f904de49a780df8da3ed98c4027
Abiria <abiria.dev@gmail.com>
2024-07-11 02:12:34 +0900

    refactor: group related classes with namespace

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -8,57 +8,59 @@ enum LogLevel {
   ERROR = 'error',
 }
 
-export class AppConfig {
-  @IsEnum(LogLevel)
-  level: LogLevel = LogLevel.INFO;
-}
+export namespace ConfigService {
+  export class App {
+    @IsEnum(LogLevel)
+    level: LogLevel = LogLevel.INFO;
+  }
 
-export class NetworkConfig {
-  @IsString()
-  host: string = 'api.teambigbox.com';
+  export class Network {
+    @IsString()
+    host: string = 'api.teambigbox.com';
 
-  @IsInt()
-  @Type(() => Number)
-  port: number = 3000;
+    @IsInt()
+    @Type(() => Number)
+    port: number = 3000;
 
-  @IsArray()
-  @IsString({ each: true })
-  cors: string[] = [];
-}
+    @IsArray()
+    @IsString({ each: true })
+    cors: string[] = [];
+  }
 
-export class AuthConfig {
-  @IsString()
-  @MinLength(4)
-  jwt_secret: string;
+  export class Auth {
+    @IsString()
+    @MinLength(4)
+    jwt_secret: string;
 
-  @IsString()
-  jwt_expiry: string = '4w';
+    @IsString()
+    jwt_expiry: string = '4w';
 
-  @IsString()
-  @MinLength(4)
-  pepper: string;
-}
+    @IsString()
+    @MinLength(4)
+    pepper: string;
+  }
 
-export class DbConfig {
-  @IsString()
-  db_host: string = 'db';
+  export class Db {
+    @IsString()
+    db_host: string = 'db';
 
-  @IsInt()
-  @Type(() => Number)
-  db_port: number = 5432;
+    @IsInt()
+    @Type(() => Number)
+    db_port: number = 5432;
 
-  @IsString()
-  db_user: string = 'postgres';
+    @IsString()
+    db_user: string = 'postgres';
 
-  @IsString()
-  @MinLength(4)
-  db_password: string;
+    @IsString()
+    @MinLength(4)
+    db_password: string;
 
-  @IsString()
-  db_database: string = 'aroundy';
-}
+    @IsString()
+    db_database: string = 'aroundy';
+  }
 
-export class KakaoApiConfig {
-  @IsString()
-  kakao_api_key: string;
+  export class KakaoApi {
+    @IsString()
+    kakao_api_key: string;
+  }
 }
