ff8c9f8e1bbcab512d83af2fb6a0bad17dd3258f
Abiria <abiria.dev@gmail.com>
2024-07-11 03:19:37 +0900

    feat: use default prisma exception filter

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
-import { CustomPrismaModule } from 'nestjs-prisma';
+import {
+  CustomPrismaModule,
+  providePrismaClientExceptionFilter,
+} from 'nestjs-prisma';
 import { prismaConfig } from './prisma.config';
 import { ConfigModule } from '@/config/config.module';
 
 @Module({
   imports: [ConfigModule, CustomPrismaModule.forRootAsync(prismaConfig)],
+  providers: [providePrismaClientExceptionFilter()],
   exports: [CustomPrismaModule], // reexport inject-container
 })
 export class PrismaModule {}
