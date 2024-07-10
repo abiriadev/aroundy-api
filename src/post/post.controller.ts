4643d4c3b56bbc686e4a4b144eec5362d77cd067
Abiria <abiria.dev@gmail.com>
2024-07-11 07:25:13 +0900

    refactor: rewrite every controller logic using new service

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -6,71 +6,32 @@ import {
   Delete,
   Body,
   Param,
-  UseInterceptors,
-  UploadedFiles,
 } from '@nestjs/common';
-import { FileFieldsInterceptor } from '@nestjs/platform-express';
 import { PostService } from './post.service';
-import { Post as PostEntity } from './post.entity';
-import { diskStorage } from 'multer';
-import { extname } from 'path';
+import { PostDto } from './post.dto';
+import { Identifiable } from '@/common/identifiable.dto';
 
 @Controller('posts')
 export class PostController {
   constructor(private readonly postService: PostService) {}
 
   @Get()
-  findAll(): Promise<PostEntity[]> {
-    return this.postService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string): Promise<PostEntity> {
-    return this.postService.findOne(id);
+  async fetch(): Promise<Array<PostDto>> {
+    return await this.postService.fetch();
   }
 
   @Post()
-  create(@Body() post: PostEntity): Promise<PostEntity> {
-    return this.postService.create(post);
+  async create(@Body() post: PostDto.Create) {
+    return await this.postService.create(post);
   }
 
   @Put(':id')
-  update(
-    @Param('id') id: string,
-    @Body() post: PostEntity,
-  ): Promise<PostEntity> {
+  async update(@Param() { id }: Identifiable, @Body() post: PostDto.Update) {
     return this.postService.update(id, post);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string): Promise<PostEntity> {
+  async remove(@Param() { id }: Identifiable) {
     return this.postService.remove(id);
-  }
-
-  @Post(':id/upload')
-  @UseInterceptors(
-    FileFieldsInterceptor([{ name: 'images', maxCount: 10 }], {
-      storage: diskStorage({
-        destination: './uploads',
-        filename: (req, file, cb) => {
-          const uniqueSuffix =
-            Date.now() + '-' + Math.round(Math.random() * 1e9);
-          cb(
-            null,
-            `${file.fieldname}-${uniqueSuffix}${extname(file.originalname)}`,
-          );
-        },
-      }),
-      limits: {
-        fileSize: 50 * 1024 * 1024, // 50MB
-      },
-    }),
-  )
-  async uploadFiles(
-    @Param('id') id: string,
-    @UploadedFiles() files: { images?: diskStorage.File[] },
-  ) {
-    const images = files.images.map((file) => `/uploads/${file.filename}`);
-    return this.postService.addImages(id, images);
   }
 }
