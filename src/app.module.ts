ee89153c8059126afacab289673ed50323619df8
Abiria <abiria.dev@gmail.com>
2024-07-11 15:28:20 +0900

    chore: register user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { TagModule } from '@/tag/tag.module';
 import { ConfigModule } from '@/config/config.module';
 import { PrismaModule } from '@/prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { AuthModule } from './auth/auth.module';
     CompanyModule,
     CategoryModule,
     TagModule,
+    UserModule,
   ],
 })
 export class AppModule {}
