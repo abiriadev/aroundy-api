35845be06c1d93d3268b18bca357d7bc9521da25
Abiria <abiria.dev@gmail.com>
2024-07-11 15:23:26 +0900

    refactor: transform posts

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,5 +1,6 @@
 import { ExtendedPrismaService } from '@/prisma/prisma.service';
 import { Inject, Injectable } from '@nestjs/common';
+import { UserDto } from './user.dto';
 
 @Injectable()
 export class UserService {
@@ -20,67 +21,81 @@ export class UserService {
     });
   }
 
-  async profile(id: string) {
-    return await this.prismaService.client.user.findUniqueOrThrow({
-      where: { uid: id },
-      select: {
-        createdAt: true,
-        updatedAt: true,
-        uid: true,
-        oauthProvider: true,
-        recentlyLoggedInAt: true,
-        savedPosts: {
-          select: {
-            id: true,
-            createdAt: true,
-            updatedAt: true,
-            title: true,
-            feeds: true,
-            caption: true,
-            contact: true,
-            publishedAt: true,
-            startedAt: true,
-            endedAt: true,
-            link: true,
-            views: true,
-            isOnline: true,
-            isOffline: true,
-            lat: true,
-            lng: true,
-            address1: true,
-            address2: true,
-            region: true,
-            branch: true,
-            tags: {
-              select: {
-                id: true,
-                name: true,
+  async profile(id: string): Promise<UserDto.Profile> {
+    const { savedPosts, ...rest } =
+      await this.prismaService.client.user.findUniqueOrThrow({
+        where: { uid: id },
+        select: {
+          createdAt: true,
+          updatedAt: true,
+          uid: true,
+          oauthProvider: true,
+          recentlyLoggedInAt: true,
+          savedPosts: {
+            select: {
+              id: true,
+              createdAt: true,
+              updatedAt: true,
+              title: true,
+              feeds: true,
+              caption: true,
+              contact: true,
+              publishedAt: true,
+              startedAt: true,
+              endedAt: true,
+              link: true,
+              views: true,
+              isOnline: true,
+              isOffline: true,
+              lat: true,
+              lng: true,
+              address1: true,
+              address2: true,
+              region: true,
+              branch: true,
+              tags: {
+                select: {
+                  id: true,
+                  name: true,
+                },
               },
-            },
-            category: {
-              select: {
-                id: true,
-                name: true,
+              category: {
+                select: {
+                  id: true,
+                  name: true,
+                },
               },
-            },
-            company: {
-              select: {
-                id: true,
-                createdAt: true,
-                updatedAt: true,
-                name: true,
-                logo: true,
+              company: {
+                select: {
+                  id: true,
+                  createdAt: true,
+                  updatedAt: true,
+                  name: true,
+                  logo: true,
+                },
               },
-            },
-            _count: {
-              select: {
-                likedUsers: true,
+              _count: {
+                select: {
+                  likedUsers: true,
+                },
               },
             },
           },
         },
-      },
-    });
+      });
+
+    return {
+      saved: savedPosts.map(
+        ({ lat, lng, _count: { likedUsers: likes }, ...rest }) => ({
+          location: lat !== null && lng !== null ? [lat, lng] : null,
+          likes,
+          liked: false,
+          saved: false,
+          ...rest,
+        }),
+      ),
+      ...rest,
+    };
   }
 
   async remove(id: string) {
