d074d3afc6c9830d84ac3455a77b800a8319a05f
Abiria <abiria.dev@gmail.com>
2024-07-11 14:43:43 +0900

    refactor: specify userDto

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,18 +1,19 @@
 import { Controller, Get, Param, Delete } from '@nestjs/common';
 import { UserService } from './user.service';
 import { Identifiable } from '@/common/identifiable.dto';
+import { UserDto } from './user.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get()
-  fetch() {
+  async fetch(): Promise<Array<UserDto>> {
     return this.userService.fetch();
   }
 
   @Delete(':id')
-  remove(@Param() { id }: Identifiable) {
+  async remove(@Param() { id }: Identifiable) {
     return this.userService.remove(id);
   }
 }
