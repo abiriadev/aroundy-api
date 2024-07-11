14c8778cd953742f5ec98543eaf8b3b1a655d774
Abiria <abiria.dev@gmail.com>
2024-07-11 14:39:42 +0900

    feat: documentize additional fields for user

--- src/user/user.dto.ts
+++ src/user/user.dto.ts
@@ -1,11 +1,19 @@
-import { Identifiable } from '@/common/identifiable.dto';
-import { IsDate, ValidateNested } from 'class-validator';
+import { IsDate, IsString, IsUUID, ValidateNested } from 'class-validator';
 import { PostDto } from '@/post/post.dto';
 
 export type Coordinate = [number, number];
 
-export class UserDto extends Identifiable {
+export class UserDto {
   static name = 'User';
+
+  // NOTE: 유저 스키마만 id가 아니라 uid를 사용해서 `Identifiable`을 상속받지 않습니다.`
+  /**
+   * 고유 ID
+   *
+   * @example 429b20b3-4df8-42da-8e40-e3816504792c
+   */
+  @IsUUID()
+  uid: string;
 
   /**
    * 해당 유저가 가입한 날짜.
@@ -18,6 +26,18 @@ export class UserDto extends Identifiable {
    */
   @IsDate()
   updatedAt: Date;
+
+  /**
+   * 가입 시 사용한 OAuth 제공업체
+   */
+  @IsString()
+  oauthProvier: string;
+
+  /**
+   * 최근 로그인 시각
+   */
+  @IsDate()
+  recentlyLoggedInAt: Date;
 }
 
 export namespace UserDto {
