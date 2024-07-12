5a90d55f3149717ce28b0d5b32d64a7f3b488838
Abiria <abiria.dev@gmail.com>
2024-07-12 16:42:20 +0900

    feat: return paginated result from controller

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -26,7 +26,7 @@ export class PostController {
   @ApiTags('App', 'Admin')
   @Get()
   @ApiOperation({ summary: '포스트 목록 조회 및 검색' })
-  async fetch(@Query() query: PostDto.Query): Promise<Array<PostDto>> {
+  async fetch(@Query() query: PostDto.Query): Promise<PostDto.Paginated> {
     return await this.postService.fetch(query);
   }
 
