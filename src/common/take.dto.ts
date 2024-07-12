448342339a8cbf27080bd5f3c4d6e222a385004a
Abiria <abiria.dev@gmail.com>
2024-07-12 17:01:19 +0900

    feat: implement `toRawTake` method for easy prisma query

--- src/common/take.dto.ts
+++ src/common/take.dto.ts
@@ -1,5 +1,7 @@
 import { IsInt, IsOptional, IsPositive, Max } from 'class-validator';
 
+export const takeLimit = 64;
+
 export class Take {
   /**
    * 한번에 총 몇 개의 항목을 가져올지 결정합니다.
@@ -10,9 +12,9 @@ export class Take {
    */
   @IsInt()
   @IsPositive()
-  @Max(64)
+  @Max(takeLimit)
   @IsOptional()
-  take?: string;
+  take?: number;
 
   /**
    * 다음 데이터를 가져올지 이전 데이터를 가져올지 페이지 방향을 결정합니다.
@@ -21,6 +23,13 @@ export class Take {
    */
   @IsOptional()
   direction?: Take.Direction;
+
+  toRawTake() {
+    return (
+      (this.take ?? takeLimit) *
+      (this.direction === Take.Direction.Backward ? -1 : 1)
+    );
+  }
 }
 
 export namespace Take {
