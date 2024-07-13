29624220ab71da675d47ee8397f492e68f5b137f
franknoh <franknoh1218@naver.com>
2024-07-13 22:51:22 +0900

    feat: change auth endpoints

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,32 +5,22 @@ import { ApiOperation, ApiTags } from '@nestjs/swagger';
 export class AuthController {
   // WARN: firebase auth를 씀에 따라 제거되거나 할 수 있습니다. 장식이라고 생각해주세요.
   /**
-   * 회원가입.
+   * 네이버 소셜 로그인.
    */
   @ApiTags('App')
-  @Post('/signup')
-  @ApiOperation({ summary: '회원가입' })
-  async signup() {
+  @Post('/naver')
+  @ApiOperation({ summary: '네이버 소셜 로그인' })
+  async naverLogin() {
     // todo!()
   }
 
   /**
-   * 로그인.
+   * 카카오 소셜 로그인.
    */
   @ApiTags('App')
-  @Post('/signin')
-  @ApiOperation({ summary: '로그인' })
-  async signin() {
-    // todo!()
-  }
-
-  /**
-   * 어드민 페이지 관리자용 로그인.
-   */
-  @ApiTags('Admin')
-  @Post('/signin/admin')
-  @ApiOperation({ summary: '관리자 로그인' })
-  async signinAdmin() {
+  @Post('/kakao')
+  @ApiOperation({ summary: '카카오 소셜 로그인' })
+  async kakaoLogin() {
     // todo!()
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,25 @@
 import { ConfigService } from '@/config/config.service';
 import { Injectable } from '@nestjs/common';
+import { initializeApp, App } from 'firebase-admin/app';
+import { credential } from 'firebase-admin';
+import { getAuth } from 'firebase-admin/auth';
 
 @Injectable()
 export class AuthService {
-  constructor(private authConfigService: ConfigService.Auth) {}
+  private app: App;
+
+  constructor(private authConfigService: ConfigService.Auth) {
+    const credentials = this.authConfigService.credential;
+    this.app = initializeApp({
+      credential: credential.cert(credentials),
+    });
+  }
 
   // TODO: use firebase admin SDK to actually verify the token
   // also, it should know which role the user has then return it
-  verifyToken(_token: string) {
-    const { firebaseClientId } = this.authConfigService;
+  async verifyToken(_token: string) {
+    const decodedToken = await getAuth(this.app).verifyIdToken(_token);
+    if (!decodedToken) return false;
 
     // NOTE: don't forget to update `recentlyLoggedInAt` field after successful signin!
 
